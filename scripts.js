b3c1da5cbc75b3d61b8ff016e322e3ca48feaa29
George Dimitrov <georgedimitrov92@gmail.com>
2020-08-31 17:30:38 +0300

    Better mobile background animation

--- scripts.js
+++ scripts.js
@@ -9,11 +9,20 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+function checkIfMobileAspectRatio(){
+    let ratio = document.documentElement.clientWidth / document.documentElement.clientHeight;
+    return (ratio < 1);
+}
+
 function duplicateImgToBg(){
     let img = document.getElementsByClassName("repeat-bg")[0];
-    let widthImg = document.documentElement.clientWidth/12;
+    let widthImg = 0;
+    if (checkIfMobileAspectRatio()){
+        widthImg = document.documentElement.clientWidth/7;
+    } else {
+        widthImg = document.documentElement.clientWidth/12;
+    }
     let bg = img.parentNode;
-    console.log(widthImg);
     img.width = widthImg;
     img.height = widthImg;
     let imgArea = img.offsetWidth * img.offsetHeight;
